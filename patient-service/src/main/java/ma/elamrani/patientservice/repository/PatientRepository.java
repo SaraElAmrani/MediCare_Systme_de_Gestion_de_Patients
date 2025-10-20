2c94f7a43cfb4b55e86fd2a75bed883bfcc589b5
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-10-20 14:39:57 +0100

    feat(patient-service): create Patient-service microservice with H2 configuration and initialization script
    - Created the Spring Boot project 'patient-service'
    - Configured in-memory H2 database
    - Added application.properties file with connection settings
    - Added data.sql file to insert patient data at startup
    - Created 'Patient' entity class
    - Added repository and DTOs for patient management
    - Tested service startup on port 4000

---
+++ patient-service/src/main/java/ma/elamrani/patientservice/repository/PatientRepository.java
@@ -0,0 +1,14 @@
+package ma.elamrani.patientservice.repository;
+
+
+import java.util.UUID;
+
+import ma.elamrani.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, UUID id);
+}
